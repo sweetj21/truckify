9e4832a226173f6546f8b54ee8117cfa649df100
Alexander Profeit <aprofeit@me.com>
2012-09-01 12:41:42 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Truckify::Application.config.secret_token = '990de2df3678b793f505dabf4822fc32be1bb7df2d3e7596659d1b91b856271c2de8e193ec6bf5f18648fbebb58cdb05520d8c486bb182340fa16c94e60a0b1f'

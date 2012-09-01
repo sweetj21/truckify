a7a5919d04e6f69472b954f5886a46459f15d504
Alexander Profeit <aprofeit@me.com>
2012-09-01 14:38:09 -0400

    choose driver by name for trip_report

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,9 @@
 class Driver < ActiveRecord::Base
   attr_accessible :first_name, :last_name
+
+  has_many :trip_reports
+
+  def first_and_last_name
+  	self.first_name + " " + self.last_name
+  end
 end
--- app/models/trip_report.rb
+++ app/models/trip_report.rb
@@ -1,3 +1,5 @@
 class TripReport < ActiveRecord::Base
   attr_accessible :driver_id, :most_distant_city, :odometer_end, :odometer_start, :trip_end_date, :trip_ended_at, :trip_start_date, :trip_started_at, :unit_id
+
+  belongs_to :driver
 end
